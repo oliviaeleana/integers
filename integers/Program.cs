c848317184dd0345f24c79f041e63faba3910145
Olivia Smith <oliviaeleanaco@gmail.com>
2019-09-05 15:44:39 -0400

    first commit

---
+++ integers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name two integers");
+
+            string value1;
+
+            string value2;
+
+            value1 = Console.ReadLine();
+
+            value2 = Console.ReadLine();
+
+            Console.WriteLine("Are your integers Equal?:");
+
+             = Console.ReadLine();
+
+            Console.WriteLine("Your two integers are not equal");
+
+            
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+    
+
+
+        }
+    }
+}
